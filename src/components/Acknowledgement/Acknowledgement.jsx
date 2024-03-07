54957a96f8f9616b1ecef369b995e8c7d282f632
ChrisChen0803 <78402800+ChrisChen0803@users.noreply.github.com>
2024-03-07 13:28:48 -0800

    123

--- src/components/Acknowledgement/Acknowledgement.jsx
+++ src/components/Acknowledgement/Acknowledgement.jsx
@@ -4,7 +4,7 @@ export const Acknowledgement = () => {
   return (
     <section id="acknowledgment" className={styles.container} >
         <h2 className={styles.ackTitle}>Acknowledgment</h2>
-        <p className={styles.ackContent}>This webpage is built based on the idea and code from the React Portfolio project by CodeCompleteYT. I would like to express our gratitude to the original creators for their work. Original Idea: <a href="https://www.youtube.com/watch?v=bmpI252DmiI" className={styles.ackContent}>React Portfolio Project</a></p>
+        <p className={styles.ackContent}>This webpage is built based on the idea and code from the React Portfolio project by CodeCompleteYT. I would like to express our gratitude to the original creators for their work. Original Idea: <a href="https://www.youtube.com/watch?v=YQCDUJ6hhNY" className={styles.ackContent}>React Portfolio Project</a></p>
     </section>
   )
 }
